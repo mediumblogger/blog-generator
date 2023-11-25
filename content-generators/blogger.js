f79fe39662bf03d05f328cc65d9f9f89d0cd3e68
harsha-bungee <anumolu@bungeetech.com>
2023-11-25 20:17:11 +0530

    added files

---
+++ content-generators/blogger.js
@@ -0,0 +1,18 @@
+const {getTopics} = require('../topic-crawlers/exploring-topics')
+
+
+
+
+
+
+// get topics from exploding topics
+
+
+
+// use ai to elaborate on topics recieved from topic crawlers
+
+
+
+
+
+// use ai to generate content based on elaborated topics recieved from ai elaboration
